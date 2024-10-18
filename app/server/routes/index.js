b6f7aaebaaf5ad084a667b08adf675369ecede75
Alessandro Bellu <alessandrobellu@hotmail.com>
2024-10-18 17:41:04 +0100

    Fixed configs

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
 
-router.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../../../build/index.html'));
-});  
+// router.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname, '../../../build/index.html'));
+// });  
 
 module.exports = router;
